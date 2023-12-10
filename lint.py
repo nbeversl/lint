e1f4be56136a7df9520af8a5219b8223cbaa5371
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-12-10 04:22:37 +0100

    initial commit of attempts to add features

--- lint.py
+++ lint.py
@@ -2,18 +2,40 @@ class UrtextLint:
 
 	name = ["LINT"]
 
+	def __init__(self, project):
+		super().__init__(project)
+		self._get_settings()
+
+	def _get_settings(self):		
+		self.settings = None
+		if '_lint' in self.project.settings:
+			if self.project.settings['_lint'] and self.project.settings['_lint'].is_node:
+				self.settings = self.project.settings['_lint'].metadata
+
+	def on_file_modified(self, filename):
+		self._get_settings()
+		if self.settings:
+			lint_on_file_modified = self.settings.get_first_value(
+				'run_when_file_modified')
+			if lint_on_file_modified != None:
+				if lint_on_file_modified.true():
+					self.run(filename)
+
 	def run(self, filename):
+		self._get_settings()		
 		contents = self.project.run_editor_method('get_buffer')
+		contents = '\n'.join(l for l in contents.split('\n') if l.strip())
 		if contents:
-			buffer = self.project.urtext_buffer(
-				self.project,
+			buffer = self.project._parse_file(
 				filename,
-				contents)
-			buffer.lex_and_parse()
+				buffer_contents=contents)
+			self._get_settings() # in case buffer is project_settings
+			if not buffer:
+				return
 			mapped_ranges = {}
 			for node in buffer.nodes:
 				for r in node.ranges:
-					if node.nested > 0:
+					if node.nested > 0 and not node.is_meta:
 						if r == node.ranges[0]: # first range
 							if node.compact:
 								r[0] = r[0] - 2 
@@ -29,26 +51,41 @@ class UrtextLint:
 						'range' : r,
 						'whitespace' : whitespace,
 						'nested' : node.nested,
-						'start' : r == node.ranges[0] and node.nested > 0
+						'start' : r == node.ranges[0] and (node.nested > 0),
+						'is_meta' : node.is_meta,
+						'end' : r == node.ranges[-1] and (node.nested > 0)
 					}
 			new_contents = []
 			range_start_positions = sorted(list(mapped_ranges.keys()))
 			for position in range_start_positions:
 				r = mapped_ranges[position]
-				range_contents = contents[r['range'][0]:r['range'][1]]
-				range_lines = [l.strip() for l in range_contents.split('\n') if l.strip()]
-				if range_lines:
-					whitespace = '\t' * r['nested']
-					whitespace += ' ' * r['whitespace']
-					whitespace_index = 0
-					if r['start']:
-						range_lines[0] = '\t' * r['nested'] + range_lines[0]
-						whitespace_index = 1
-					if len(range_lines) > 1:
-						range_lines[whitespace_index:] = [whitespace + l for l in range_lines[whitespace_index:]]
-					new_contents.extend(range_lines)
-				# print([whitespace + l for l in range_lines])
-			new_contents = '\n'.join(new_contents)
+				if not r['is_meta']:
+					range_contents = contents[r['range'][0]:r['range'][1]]
+					range_lines = [l.strip() for l in range_contents.split('\n')]
+					if range_lines:
+						whitespace = '\t' * r['nested']
+						whitespace += ' ' * r['whitespace']
+						whitespace_index = 0
+						if r['start'] or r['end']:
+							spaces = 1
+							if self.settings:
+								spaces = self.settings.get_first_value(
+									'space_between_nodes')
+								if spaces:
+									spaces = int(spaces.num())
+								else: 
+									spaces = 1
+							range_lines[0] = '\t' * r['nested'] + range_lines[0]
+							whitespace_index = 1
+						if len(range_lines) > 1:
+							range_lines[whitespace_index:] = [
+								whitespace + l for l in range_lines[whitespace_index:]]
+					new_contents.append('\n'.join(range_lines))
+				else:
+					new_contents.append(
+						contents[r['range'][0]-2:r['range'][1]+1]
+						)
+			new_contents = ''.join(new_contents)
 			buffer._set_contents(new_contents)
 
 urtext_extensions = [UrtextLint]
