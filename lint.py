c0b466bc85f1502679ecaf929a96b92c5ed405e1
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-12-10 01:35:10 +0100

    re-implement as dict; add leading whitespace to all subsequent lines;

--- lint.py
+++ lint.py
@@ -11,33 +11,43 @@ class UrtextLint:
 				contents)
 			buffer.lex_and_parse()
 			mapped_ranges = {}
-			all_ranges = []
 			for node in buffer.nodes:
-				mapped_ranges.setdefault(node.nested, [])
-				ranges = node.ranges
-				for r in ranges:
+				for r in node.ranges:
 					if node.nested > 0:
-						if r == ranges[0]: # first range
+						if r == node.ranges[0]: # first range
 							if node.compact:
 								r[0] = r[0] - 2 
 							else:
 								r[0] = r[0] - 1
-						if r == ranges[-1]: # last range
+							whitespace = (len(contents[r[0]+1:r[1]]) - len(contents[r[0]+1:r[1]].lstrip())) + 1
+						if r == node.ranges[-1]: # last range
 							if not node.compact:
 								r[1] = r[1] + 1
-					mapped_ranges[node.nested].append(r)
-					all_ranges.append(r)
+					else:
+						whitespace = 0
+					mapped_ranges[r[0]] = {
+						'range' : r,
+						'whitespace' : whitespace,
+						'nested' : node.nested,
+						'start' : r == node.ranges[0] and node.nested > 0
+					}
 			new_contents = []
-			all_ranges = sorted(
-				all_ranges,
-				key = lambda r : r[0])
-			for r in all_ranges:
-				for nested in mapped_ranges:
-					if r in mapped_ranges[nested]:
-						break
-				range_contents = contents[r[0]:r[1]]
+			range_start_positions = sorted(list(mapped_ranges.keys()))
+			for position in range_start_positions:
+				r = mapped_ranges[position]
+				range_contents = contents[r['range'][0]:r['range'][1]]
 				range_lines = [l.strip() for l in range_contents.split('\n') if l.strip()]
-				new_contents.extend(['\t' * nested + l for l in range_lines] )
+				if range_lines:
+					whitespace = '\t' * r['nested']
+					whitespace += ' ' * r['whitespace']
+					whitespace_index = 0
+					if r['start']:
+						range_lines[0] = '\t' * r['nested'] + range_lines[0]
+						whitespace_index = 1
+					if len(range_lines) > 1:
+						range_lines[whitespace_index:] = [whitespace + l for l in range_lines[whitespace_index:]]
+					new_contents.extend(range_lines)
+				# print([whitespace + l for l in range_lines])
 			new_contents = '\n'.join(new_contents)
 			buffer._set_contents(new_contents)
 
