621cb0f9994ea4fe917a3e88af72c9d17314cb8a
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-12-14 17:27:05 +0100

    whitestrips contents conditionally by range rather than all at once; code cleanup

--- lint.py
+++ lint.py
@@ -23,21 +23,23 @@ class UrtextLint:
 
 	def run(self, filename):
 		self._get_settings()		
-		contents = self.project.run_editor_method('get_buffer')
-		contents = '\n'.join(l for l in contents.split('\n') if l.strip())
+		contents = self.project.run_editor_method('get_buffer', filename)
+		if not contents:
+			return
+		contents = '\n'.join(l for l in contents.split('\n'))
 		if contents:
 			buffer = self.project._parse_file(
 				filename,
 				buffer_contents=contents)
-			self._get_settings() # in case buffer is project_settings
 			if not buffer:
 				return
 
 			# calculate params for every range of the file
+			self._get_settings() # in case buffer is a project_settings node
 			mapped_ranges = {}
 			for node in buffer.nodes:
-				whitespace = 0
 				for r in node.ranges:
+					whitespace = 0
 					if node.nested > 0 and not node.is_meta:
 						if r == node.ranges[0]: # first range
 							if node.compact:
@@ -45,15 +47,10 @@ class UrtextLint:
 							else:
 								r[0] = r[0] - 1
 							first_line = contents[r[0]+1:r[1]].split('\n')[0]
-							if first_line.strip():
-								whitespace = (len(first_line) - len(first_line.lstrip())) + 1
-							else:
-								whitespaces = 0
+							whitespace = (len(first_line) - len(first_line.lstrip())) + 1
 						if r == node.ranges[-1]: # last range
 							if not node.compact:
 								r[1] = r[1] + 1
-					else:
-						whitespace = 0
 					mapped_ranges[r[0]] = {
 						'range' : r,
 						'whitespace' : whitespace,
@@ -78,13 +75,10 @@ class UrtextLint:
 						if r['start']: # first range
 							spaces_between_nodes = 1
 							if self.settings:
-								spaces_setting = self.settings.get_first_value(
-									'space_between_nodes')
+								spaces_setting = self.settings.get_first_value('space_between_nodes')
 								if spaces_setting:
 									spaces_between_nodes = int(spaces_setting.num())
-								else: 
-									spaces_between_nodes = 1
-							range_lines[0] = ('\n' * spaces_between_nodes) + '\t' * r['nested'] + range_lines[0]
+							range_lines[0] = ('\n' * (spaces_between_nodes + 1)) + '\t' * r['nested'] + range_lines[0]
 							whitespace_start_index = 1
 						if len(range_lines) > 1:
 							for index, line in enumerate(range_lines):
@@ -96,11 +90,17 @@ class UrtextLint:
 										range_lines[index] = tabs + range_lines[index]
 									else:
 										range_lines[index] = tabs + whitespace + range_lines[index]
+						for line in reversed(range_lines): 
+							if line.strip():
+								break
+							range_lines.pop()
 					new_contents.append('\n'.join(range_lines))
 				else:
 					new_contents.append(
-						contents[r['range'][0]-2:r['range'][1]+1]
-						)
+						contents[
+						r['range'][0]-2
+						:
+						r['range'][1]+1])
 			new_contents = ''.join(new_contents)
 			buffer._set_contents(new_contents)
 
